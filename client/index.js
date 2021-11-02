b5804f6258af7ce335bd9143d5e1d66397db57f0
Zack Ward <wardzb7@gmail.com>
2021-11-01 21:00:52 -0400

    added redux boilerplate

--- client/index.js
+++ client/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store/index';
 
 ReactDOM.render(
-  <div>Hello, world!</div>,
+  <Provider store={store}>
+    {/* rest of app goes here */}
+  </Provider>
   document.querySelector('#app')
 );
